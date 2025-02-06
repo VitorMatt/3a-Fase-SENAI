ef0c3d7dc7410868684e01fb3527c39dc9e9e7bf
VitorMatt <vitor.azevedoo2812@gmail.com>
2025-02-06 17:22:46 -0300

    Teste

---
+++ revisao/src/components/Body.jsx
@@ -0,0 +1,11 @@
+import './CSS/Body.css';
+
+function Body() {
+  return (
+    <div className="body">
+        <p>Empresa de LOGOS</p>
+    </div>
+  );
+};
+
+export default Body;
---
+++ revisao/src/components/Header.jsx
@@ -0,0 +1,21 @@
+import './CSS/Header.css';
+
+function Header() {
+
+  return (
+    <div className="header">
+      <div className='header2'>
+        <p className="logo">Logo</p>
+        <ol>
+          <ul>Link</ul>
+          <ul>Link</ul>
+          <ul>Link</ul>
+          <ul>Link</ul>
+          <ul>Link</ul>
+        </ol>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
